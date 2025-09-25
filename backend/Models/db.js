8579b122dc9b6ed085c7b7f2e44c3bab7774db83
kranthiuppada <kranthi.uppada123@gmail.com>
2025-09-26 00:54:00 +0530

    db-updated

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-const DB_URL =
+const dotenv = require('dotenv');
+dotenv.config();
+const DB_URL =process.env.MONGO_URL;
 
 mongoose.connect(DB_URL)
     .then(() => {
